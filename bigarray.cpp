90807afe92fd11a943c7348d4fbdb9446efaa219
Brett Viren <brett.viren@gmail.com>
2018-01-31 17:54:56 -0500

    Various, moving some stuff to blips.

--- bigarray.cpp
+++ bigarray.cpp
@@ -8,23 +8,27 @@ int main()
     const size_t nticks = 3*2e6; // 3 seconds * 2 MHz
     const size_t nsamples = nchan*nticks;
 
+
     auto start = std::chrono::high_resolution_clock::now();
-    short* a = new short[nsamples];
+    short* rawbuf = new short[nsamples];
     auto stop = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_seconds = stop-start;
-    std::cerr << "allocate in " << elapsed_seconds.count() << std::endl;
+    std::cerr << "allocate " << nchan << " x " << nticks
+              << " samples = " << nsamples / 1e9 << " GS in " << elapsed_seconds.count() << std::endl;
+
+
 
     start = std::chrono::high_resolution_clock::now();
-    std::valarray<short> buf(a, nsamples);
+    std::valarray<short> buf(rawbuf, nsamples);
     for (size_t ich=0; ich<nchan; ++ich) {
-	std::valarray<short> bytick = buf[std::slice(ich*nticks, nticks, 1)];
-	for (size_t ind=0; ind<nticks; ++ind) {
-	    bytick[ind] = 0;
-	}
+        std::valarray<short> bytick = buf[std::slice(ich*nticks, nticks, 1)];
+        for (size_t ind=0; ind<nticks; ++ind) {
+            bytick[ind] = 0;
+        }
     }
     stop = std::chrono::high_resolution_clock::now();
     elapsed_seconds = stop-start;
-    std::cerr << "loop with stride in " << elapsed_seconds.count() << std::endl;
+    std::cerr << "zero by loop with stride in " << elapsed_seconds.count() << std::endl;
 
 
     std::valarray<short> chsum((short)0, nchan);
@@ -32,13 +36,13 @@ int main()
 
     start = std::chrono::high_resolution_clock::now();
     for (size_t ind=0; ind<nticks; ++ind) {
-	std::valarray<short> bych(a + (ind*nchan), nchan);
+	std::valarray<short> bych(rawbuf + (ind*nchan), nchan);
 	chsum += bych;
 	chsum2 += bych*bych;
     }
     stop = std::chrono::high_resolution_clock::now();
     elapsed_seconds = stop-start;
-    std::cerr << "loop by tick in " << elapsed_seconds.count() << std::endl;
+    std::cerr << "mean/rms loop by parallel tick in " << elapsed_seconds.count() << std::endl;
 
     return 0;
 }
--- wscript
+++ wscript
@@ -5,11 +5,38 @@ def options(opt):
 def configure(conf):
     conf.load('boost', tooldir="waftools")
     conf.load('compiler_cxx')
-    conf.env.CXXFLAGS += ["-O3", "-std=c++14"]
+    #conf.env.CXXFLAGS += ["-O3", "-std=c++14"]
+    conf.env.CXXFLAGS += ["-g", "-std=c++14"]
     conf.check_boost(lib='timer system')
+    conf.check_cfg(package='eigen3',  uselib_store='EIGEN', args='--cflags --libs')
 
 
 def build(bld):
+    bld.program(source='bigarray.cpp', 
+                target='bigarray',
+                includes=".",
+                #lib = ["boost_thread", "boost_system", "pthread"],
+                #use='BOOST'
+    )    
+    bld.program(source='test_nparray.cpp cnpy.cxx', 
+                target='test_nparray',
+                includes=".",
+                lib = ["z"],
+                use='EIGEN'
+    )    
+    bld.program(source='test_cnpy.cpp cnpy.cxx', 
+                target='test_cnpy',
+                includes=".",
+                lib = ["boost_thread", "boost_system", "pthread","z"],
+                use='EIGEN BOOST',
+    )    
+    bld.program(source='maparray.cpp', 
+                target='maparray',
+                includes=".",
+                lib = ["boost_thread", "boost_system", "pthread","rt"],
+                use=['EIGEN','BOOST']
+    )    
+
     bld.program(source='test_arene.cxx', 
                 target='test_arene',
                 includes=".",
